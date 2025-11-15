a988e78c9c17248c2b5da5202d0534dfd3cc6c13
orhan <orhantrkmn749@gmail.com>
2025-11-15 21:18:44 +0300

    Initial project setup

---
+++ src/main/java/com/canbagi/CanBagiApplication.java
@@ -0,0 +1,13 @@
+package com.canbagi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanBagiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanBagiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canbagi/CanBagiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canbagi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanBagiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
